d3c05c58e1764f4e5f6a6dce8387ff1395c6e152
alexmunoz05 <71845532+alexmunoz05@users.noreply.github.com>
2021-02-05 17:51:35 -0700

    Practica1

---
+++ funcional/introduccion/practica1.hs
@@ -0,0 +1,36 @@
+suma1 a b = a + b
+suma2 a b c d e = a + b + c + d + e
+
+resta1 a b = a - b
+resta2 a b c d = a - b - c - d
+
+multi1 a b c = a * b * c 
+multi2 a b c d e = a * b * c * d * e 
+
+division1 a b c d = a / b / c / d
+division2 a b c d e = a / b / c / d / e
+
+division3 a b = a `div` b
+division4 a b c = a `div` b `div` c 
+
+modulo1 a b = a `mod` b
+modulo2 a b c = a `mod` b `mod` c 
+
+pregunta1 a = not a
+pregunta2 b = not b 
+
+concatenar1 = "Peje" ++ "lagarto"
+concatenar2 = "Has" ++ "kell"
+
+comparasion1 a b = a == b
+comparasion2 a b = a /= b
+
+comparasion3 a b = a && b
+comparasion4 a b c = a && b == c
+
+comparasion5 a b = a || b
+comparasion6 a b c =  a || b /= c
+
+
+
+ 
