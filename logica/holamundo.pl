9439969a01acda7543969ec2eb11d9843ffe7c6c
alexmunoz05 <71845532+alexmunoz05@users.noreply.github.com>
2021-02-05 20:43:40 -0700

    holamundo

---
+++ logica/holamundo.pl
@@ -0,0 +1,23 @@
+valioso(oro).
+valioso(cobre).
+es_libro(cienanios).
+
+progenitor(clara,jose).
+progenitor(tomas,jose).
+progenitor(tomas,isabel).
+progenitor(jose,ana).
+progenitor(jose,patricia).
+progenitor(patricia,jaime).
+
+hombre(jose).
+hombre(tomas).
+hombre(jaime).
+
+mujer(clara).
+mujer(isabel).
+mujer(ana).
+mujer(patricia).
+
+%tia
+tia(X,Y):- progenitor(Z,X), progenitor(Z,A), progenitor(A,Y), mujer(X).
+abuela(X,Y):- progenitor(X,Z), progenitor(Z,Y), mujer(X).
