bb6b54602d2ddda7b02f92c817aa80639305dc2a
alexmunoz05 <71845532+alexmunoz05@users.noreply.github.com>
2021-02-05 18:16:54 -0700

    Practica2

---
+++ funcional/introduccion/funcionespredefinidas.hs
@@ -0,0 +1,28 @@
+funcion1 x = abs x
+
+funcion2 x = ceiling x
+
+funcion3 x = succ x
+
+funcion4 x y = min x y
+
+funcion5 x y = max x y
+
+funcion6 x y = div x y
+
+funcion7 x y = mod x y
+
+funcion8 x = even x
+
+funcion9 x = floor x
+
+funcion10 x y = gcd x y
+
+funcion11 x = not x
+
+funcion12 x = odd x
+
+funcion13 x = signum x
+
+funcion14 x y = snd (x,y)
+
